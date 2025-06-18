d69bd962f0e65ab810c0bae1adaeb484458dd7a1
Sasitha Madushaka <sasithamadushanka@gmail.com>
2025-06-18 21:54:45 +1200

    hero update

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -13,17 +13,17 @@ export const Hero = () => {
                         Sasitha Madushanka
                     </h1>
                     <h1 className="text-1xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                        A Full Stack Developer
+                    Technical Lead with 10+ years crafting enterprise applications on AWS. I transform complex business challenges into elegant, performant software
                     </h1>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
                         <a
                             href="#about_me"
                             className="rounded-md bg-bramble-700 px-3.5 py-1.5 text-xs lg:text-base font-semibold leading-7 text-white shadow-sm hover:bg-bramble-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bramble-600"
                         >
-                            Contact Me
+                            Let's Connect
                         </a>
                         <a href="#projects" className=" text-sm lg:text-base font-semibold leading-7 text-gray-900">
-                            Learn More
+                            View My Work
                         </a>
                     </div>
                 </div>
